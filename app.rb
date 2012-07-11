a58b101cbe5fea13dc12bd935e7db9c8750752ff
massat <mail@massat.jp>
2012-07-12 01:01:21 +0900

    adaptive for jsonp

--- app.rb
+++ app.rb
@@ -1,15 +1,31 @@
 
-get '/members' do
+@@formats = {:json => :json, :jsonp => :js }
 
+
+get '/members.?:format?' do
+
+  # determines format
+  format  = params[:format] || :json
+  format  = format.to_sym
+
+  if !@@formats.has_key?(format)
+    halt
+  end
+
+  content_type @@formats[format]
+
+
+  # fetch members
   db = Mongo::Connection.new.db('ville')
-
-  members    = db.collection('member').find.to_a
-
-  content_type :json
-  JSON.unparse(members)
-
-end
+  @members = db.collection('member').find.to_a
 
 
-get '/works' do
+  case format
+  when :json
+    JSON.unparse(@members)
+  when :jsonp
+    callback = params[:callback] || 'alert'
+    "<script type=\"text/javascript\">#{callback}(#{JSON.unparse(@members)})</script>"
+  end
+
 end
