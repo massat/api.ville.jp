b4f38a8c43ad86a4810488226f5c5b4c3568bf95
massat <mail@massat.jp>
2012-07-12 01:52:23 +0900

    add api to retrieve a member

--- app.rb
+++ app.rb
@@ -1,31 +1,55 @@
 
-@@formats = {:json => :json, :jsonp => :js }
+
+helpers do
+
+  # output renderer
+  def render(format, content)
+
+    formats = {:json => :json, :jsonp => :js }
+
+    if !formats.has_key?(format)
+      halt 400
+    end
+
+    content_type formats[format]
+
+    case format
+    when :json
+      JSON.unparse(content)
+    when :jsonp
+      callback = params[:callback] || 'alert'
+      "<script type=\"text/javascript\">#{callback}(#{JSON.unparse(content)})</script>"
+    end
+  end
+
+end
 
 
 get '/members.?:format?' do
 
-  # determines format
-  format  = params[:format] || :json
-  format  = format.to_sym
-
-  if !@@formats.has_key?(format)
-    halt
-  end
-
-  content_type @@formats[format]
-
+  format = params[:format] || :json
+  format = format.to_sym
 
   # fetch members
-  db = Mongo::Connection.new.db('ville')
-  @members = db.collection('member').find.to_a
+  db      = Mongo::Connection.new.db('ville')
+  members = db.collection('member').find.to_a
+
+  render(format, members)
+end
 
 
-  case format
-  when :json
-    JSON.unparse(@members)
-  when :jsonp
-    callback = params[:callback] || 'alert'
-    "<script type=\"text/javascript\">#{callback}(#{JSON.unparse(@members)})</script>"
-  end
+get %r{/member/([a-zA-Z0-9]+)(\.(.+))?} do
 
+  slug   = params[:captures].first
+
+  format = params[:captures][2] || :json
+  format = format.to_sym
+
+  # fetch members
+  db     = Mongo::Connection.new.db('ville')
+  member = db.collection('member').find_one({:name => slug})
+
+  halt 404 if member.empty?
+
+  render(format, member)
 end
